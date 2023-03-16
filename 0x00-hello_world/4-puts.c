bd59961e8e0bf21ccc632606d16ad8cdf660b490
Sherrif25 <johnmugo2019@gmail.com>
2023-03-16 15:02:30 -0700

    print to stdout using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - function  to print using puts
+ * Return: 0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
