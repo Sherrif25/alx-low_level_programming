de60e5326d6675b2f3bcf615b3b6029f0bb063b0
Sherrif25 <johnmugo2019@gmail.com>
2023-03-27 11:29:22 -0700

    100-atoi.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * char *_strcpy -a function that copies the string pointedto by src
+ * @dest: copy to
+ * @src: copy from
+ * Return: string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int | = 0;
+	int x = 0;
+
+	while (*(src + |) != '\0')
+	{
+		|++;
+	}
+	for ( ; x < | ; x++)
+	{
+		dest[x] = src[x];
+	}
+	dest [|] = '\0';
+	return (dest);
+}
